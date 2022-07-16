37693ca575b3d92582c6bac31c5da6279cc82daf
codexfast <codexfast@gmail.com>
2022-07-16 16:13:36 -0300

    some fix & some changes

--- upInstall.py
+++ upInstall.py
@@ -2,9 +2,11 @@
 
 import urllib.request
 import zipfile
+import shutil
 
 REPO = "https://github.com/codexfast/pixhos/archive/refs/heads/main.zip"
-TARGET = "/temp"
+TARGET = "./temp"
+FOLDER_INSTALL = "./src"
 FILE = "pixhos.zip"
 
 def getPixhos() -> urllib:
@@ -13,6 +15,11 @@ def getPixhos() -> urllib:
 def install(res: urllib): # Update/install
     with zipfile.ZipFile(FILE, "r") as z_ref:
         z_ref.extractall(TARGET)
+
+    shutil.rmtree(FOLDER_INSTALL)
+    shutil.move(TARGET+'/pixhos-main/src', './src')
+    shutil.rmtree(TARGET)
+    
 
 if __name__ == "__main__":
     try:
